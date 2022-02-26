1eaa9ecb32b8997a584640c511044db5162a4672
unknown <alejandrojzeballos@gmail.com>
2022-02-26 15:48:54 -0400

    {add} añadiendo composicion en app

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,58 @@
 import React from 'react';
 import { TodoProvider } from '../TodoContext';
+import { TodoItem } from '../TodoItem';
 import { AppUI } from './AppUI';
 
+function App(){
+  const [state, setState] = React.useState('Estado compartido');
+  return (
+      <React.Fragment>
+        <TodoHeader>
+            <TodoCounter/>
+            <TodoSearch/>
+        </TodoHeader>
 
-function App() {
+        <TodoList>
+          <Todoitem state={state}/>
+        </TodoList>
+      </React.Fragment>
+  );
+}
+
+function TodoHeader({children}){
+  return (
+    <header>
+        {children}
+    </header>
+  );
+}
+
+function TodoList({children}){
+  return (
+    <section className="TodoList-container">
+      {children}       
+    </section>
+  );
+}
+
+function TodoCounter(){
+  return <p>TodoCounter</p>; 
+}
+
+function TodoSearch(){
+  return <p>TodoSearch</p>;
+}
+
+function Todoitem({state}){
+  return <p>TodoItem {state}</p>;
+}
+
+/* function App() {
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-}
+} */
 
 export default App;
