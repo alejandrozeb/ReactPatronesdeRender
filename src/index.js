892ba8e8bf87672e84b6245c593f6fa3aef3105b
unknown <alejandrojzeballos@gmail.com>
2022-03-04 13:41:58 -0400

    {add} high order components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+//import App from './App/index.js';
 import './index.css';
+function App(props) {
+  return (
+    <h1>{props.saludo}, {props.saludo}</h1>
+  );
+}
+
+function withWhatever(WarppedComponent){
+  return function ComponenteDeverdad(props){
+    return (
+      <React.Fragment>
+        <WarppedComponent {...props}/>
+        <p>Estamos acompañando al wrappedComponent</p>
+      </React.Fragment>
+    );
+  }
+}
+
+const AppwithWhatever = withWhatever(App);
+
 
 ReactDOM.render(
-  <App />,
+  <AppwithWhatever saludo="Buenas" nombre="Ale"/>,
+  //<App saludo="Buenas" nombre="Ale" />,
   document.getElementById('root')
 );
