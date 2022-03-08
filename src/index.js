8c4a03aed689b5fbd4888913ed318d8d9c4986c2
unknown <alejandrojzeballos@gmail.com>
2022-03-07 20:08:23 -0400

    {add}añadiendo change alert

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App/index.js';
+import App from './App/index.js';
 import './index.css';
-function App(props) {
+
+ReactDOM.render(
+  <App/>,
+  //<App saludo="Buenas" nombre="Ale" />,
+  document.getElementById('root')
+)
+
+
+/* function App(props) {
   return (
     <h1>{props.saludo}, {props.saludo}</h1>
   );
-}
+} */
 
 /* function withWhatever(WarppedComponent){
   return function ComponenteDeverdad(props){
@@ -19,7 +27,7 @@ function App(props) {
   }
 } */
 
-function withSaludo(WrappedComponent) { 
+/* function withSaludo(WrappedComponent) { 
   return function WarppedComponentWithSaludo (saludo){
     return function ComponenteDeverdad(props){
       return (
@@ -30,14 +38,15 @@ function withSaludo(WrappedComponent) {
       );
   }
  }
-}
+} */
 
 //const AppwithWhatever = withWhatever(App);
-const AppwithWhatever = withSaludo(App)('Buenas');
+//const AppwithWhatever = withSaludo(App)('Buenas');
 
 
-ReactDOM.render(
+
+/* ReactDOM.render(
   <AppwithWhatever nombre="Ale"/>,
   //<App saludo="Buenas" nombre="Ale" />,
   document.getElementById('root')
-)
+) */
